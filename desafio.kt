25ef0bf10cd05d7089d649879b08933c23b68424
Raylander Guimarães <92590610+raylanderguimaraes@users.noreply.github.com>
2023-06-13 16:39:33 -0300

    Primeiro Commit

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,4 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+data class Usuario(val nome: String, val idade: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -16,6 +12,10 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //
+    
+    val user1 = Usuario("Raylander", 30)
+    println(user1.toString())
 }
