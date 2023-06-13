4284015204c8b029ce3b5a1d04eb6eb19023eb04
Raylander Guimarães <92590610+raylanderguimaraes@users.noreply.github.com>
2023-06-13 19:50:29 -0300

    desafio finalizado

--- desafio.kt
+++ desafio.kt
@@ -1,13 +1,14 @@
 data class Usuario(val nome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
@@ -16,6 +17,25 @@ fun main() {
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
     //
     
-    val user1 = Usuario("Raylander", 30)
-    println(user1.toString())
+    // Cria dois usuários da plataforma de ensino
+    val usuario1 = Usuario("Raylander", 30)
+    val usuario2 = Usuario("Jocastha", 31)
+    
+    // cria lista de conteudos da formacao
+    val conteudosFormacao: MutableList<ConteudoEducacional> = mutableListOf(
+    		ConteudoEducacional("Lógica de Programação"),
+        	ConteudoEducacional("Estruturas condicionais e de repetição")
+    )
+    
+    //cria formacao com conteudos
+    val formacao = Formacao("Curso básico de Java", conteudosFormacao)
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    
+    //mostra a lista de usuarios inscritos na formacao
+    for(usuario in formacao.inscritos) {
+        println("Nome: ${usuario.nome}, Idade: ${usuario.idade}, Conteudos: ${conteudosFormacao} ")
+    }
+    
 }
